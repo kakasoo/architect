1ea793d18b73500bafebca315ae83a63a7cd6577
kakasoo <kscodebase@gmail.com>
2021-05-11 22:41:38 +0900

    [강구조] 집중하중 하의 단순보의 Cb 산정

--- steelStructure/chapter7/Cb.js
+++
@@ -1,9 +0,0 @@
-const getCb = () => {
-    const Ma = 7 / 128;
-    const Mb = 3 / 32;
-    const Mc = 15 / 128;
-    const Mmax = 1 / 8;
-    const Cb = (12.5 * Mmax) / (2.5 * Mmax + 3 * Ma + 4 * Mb + 3 * Mc);
-    console.log(Cb);
-};
-getCb();
---
+++ steelStructure/chapter7/Cb2.js
@@ -0,0 +1,38 @@
+// 175P의 Cb 값 구하기
+
+const getCb = (Ma, Mb, Mc, Mmax) => {
+    const Cb = (12.5 * Mmax) / (2.5 * Mmax + 3 * Ma + 4 * Mb + 3 * Mc);
+    return Cb;
+};
+
+const printAnswer = (Ma, Mb, Mc, Mmax) => {
+    console.log("Ma : ", Ma);
+    console.log("Mb : ", Mb);
+    console.log("Mc : ", Mc);
+    console.log("Mmax : ", Mmax);
+    console.log("Cb : ", getCb(Ma, Mb, Mc, Mmax));
+};
+
+집중하중_하의_단순보의_Cb값 = (event) => {
+    (L = 1), (P = 1), (PL = L * P); // 여기서의 L은 Lb이다.
+
+    if (event === "a") {
+        const Ma = (P / 2) * (P / 4); //  PL/8
+        const Mb = (P / 2) * (L / 2); // PL/4
+        const Mc = Ma;
+        const Mmax = Mb;
+
+        printAnswer(Ma, Mb, Mc, Mmax);
+    }
+
+    if (event === "b") {
+        const Ma = PL / 16;
+        const Mb = PL / 8;
+        const Mc = (3 * PL) / 16;
+        const Mmax = PL / 4;
+
+        printAnswer(Ma, Mb, Mc, Mmax);
+    }
+};
+
+집중하중_하의_단순보의_Cb값("a");
